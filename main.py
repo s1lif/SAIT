e4b6d6e75b0a2055b2e2e26b70c080bdb686798d
s1lif <83955148+s1lif@users.noreply.github.com>
2022-02-26 17:20:47 +0000

    q2e1qewq

--- main.py
+++ main.py
@@ -18,3 +18,7 @@ if os.environ.get('APP_LOCATION') == 'heroku':
 	run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 else:
 	run(host='localhost', port=8080, debug=True)
+  
+  @route('/')
+def root():
+    return static_file('root.html', 'static/')
